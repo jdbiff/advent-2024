25c967d8dc67a6b311c07892883ee33f4b5ccbda
Jordan Bithell <jdbiff@gmail.com>
2024-12-03 01:22:36 +0000

    Day 2: Cleanup
    Cleaned up code

--- Day 2/main.js
+++ Day 2/main.js
@@ -1,6 +1,5 @@
 import * as FS from "fs";
-import { partOne } from "./partOne.js";
-import { partTwo } from "./partTwo.js";
+import { any, dampen, isSafe } from "./funcs.js";
 
 //let data = FS.readFileSync("example.txt", "utf8");
 let data = FS.readFileSync("input.txt", "utf8");
@@ -10,6 +9,18 @@ data = data.trim().split("\n");
 let sets = data
   .map((e) => e.split(" "))
   .map((set) => set.map((val) => parseInt(val)));
+
+function partOne(sets) {
+  return sets.map((set) => isSafe(set)).reduce((a, b) => a + b, 0);
+}
+
+function partTwo(sets) {
+  let damped = sets.map(
+    (set) => isSafe(set) || any(dampen(set), (damped) => isSafe(damped)),
+  );
+
+  return damped.reduce((a, b) => a + b, 0);
+}
 
 console.log("p1", partOne(sets));
 console.log("p2", partTwo(sets));
